49795d830a5c5ea9a71cccb5f998c7db007f97ad
kstrubel <kstrubel@purdue.edu>
2016-12-21 20:25:35 -0500

    Update MainModule
    broke the functions into more tasks, got rid of "continue" used in functions

--- mainModulePedalBox.c
+++ mainModulePedalBox.c
@@ -17,11 +17,11 @@
 #define MAX_BRAKE_LEVEL 42.42424242 // BOGUS VALUE
 #define MAX_THROTTLE_LEVEL 42.42424242 // BOGUS VALUE
 #define LC_THRESHOLD 10 // BOGUS VALUE
-xQueueHandle Global_Queue_Handle = 0; 
+xQueueHandle Global_Pedal_Box_Queue_Handle = 0; 
 
 typedef struct _pedalbox_msg {
-   bool EOR;
-   bool APPS_Implausible;
+   bool EOR; // rule 2.4.6: Encoder out of range
+   bool APPS_Implausible; // rule 2.3.5
    float throttle_level;
    float brake_level;
    
@@ -51,7 +51,8 @@ int pedalBoxMsgHandler() {
 *		2.brake_flag
 *		3.MMPB_TIME time this function was last run 
 *		4.current_time
-*		5.Gloabal_Queue_Handle
+*		5.Global_Pedal_Box_Queue_Handle
+*		6.g_throttle_level
 *
 *     Function Description:
 *			Takes input from pedal box, runs safetly check, sets torque
@@ -59,26 +60,47 @@ int pedalBoxMsgHandler() {
 	while (1) {
 		Pedalbox_msg_t msg;
 
-		if(xQueueReceive(Gloabal_Queue_Handle, &msg, 1000)){
+		if(xQueueReceive(Global_Pedal_Box_Queue_Handle, &msg, 1000)){
+
 			// set function time stamp
 			MMPB_TIME = current_time;
 
-			if (msg.EOR) { 
+			if (msg.EOR) { // rule 2.4.6
 				torque = 0;
 				// send CAN message to motor, set to Low Power State
 				// sendMotorMessage();
 
-				// Set Pin ? to high
-				/* TODO */
-				continue;
+				// toggle frg/run fre
+				HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_11);
+				HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_12);
+				
+				// pin 29 port e11
+				// pin 30 port e12
+				
+				
 			} 
+			
 			if (msg.APPS_Implausible) {
 				torque = 0;
-				continue;
+				// send CAN message to motor, set to Low Power State
+				// sendMotorMessage();
+
+				// toggle frg/run fre
+				HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_11);
+				HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_12);
+				// pin 29 port e11
+				// pin 30 port e12
+				/* TODO */
+			
 			}
 			if (msg.brake_level > 0.05*MAX_BRAKE_LEVEL) {
 				// signal LV to turn on break light
-				HAL_CAN_TRANSMIT()
+				// Brake light 
+				
+			
+				HAL_GPIO_WritePin(GPIOP, GPIO_PIN_E7, GPIO_PIN_SET);
+				// set pin PE7
+
 				/* TODO */
 
 				if (msg.throttle_level > 0.25 * MAX_THROTTLE_LEVEL) {
@@ -86,18 +108,23 @@ int pedalBoxMsgHandler() {
 				}
 			}
 
+			else {
+				// turn Brake light off
+				HAL_GPIO_WritePin(GPIOP, GPIO_PIN_E7, GPIO_PIN_RESET);
+			}
+
 			if (brake_flag) {
 				if(msg.throttle_level > 0.05 * MAX_THROTTLE_LEVEL){
 					torque = 0;
-					continue;
 				}
 				brake_flag = false;
 			}
 
 			// set throttle
-			torque = throttle_level;
+			g_throttle_level = msg.throttle_level;
+			torque = msg.throttle_level;
 
-			continue;	
+				
 		}
 	}
 		
@@ -105,12 +132,12 @@ int pedalBoxMsgHandler() {
 
 }
 
-int mainModuleIdle() {
+int mainModuleTorque() {
 /***************************************************************************
 *
 *     Function Information
 *
-*     Name of Function: mainModuleIdle
+*     Name of Function: mainModuleTorque
 *
 *     Programmer's Name: Kai Strubel
 *
@@ -122,15 +149,12 @@ int mainModuleIdle() {
 *      Global Dependents:
 *	    1.bool launchControl
 *		2.float apps_max
-*		3.float MMPB_TIME time pedal box message handler function was last run 
-*		4.float MMWM_TIME time wheel module handler function was last run
-*		5.float torque
-*		6.float scale factor
-*		7.float currentTime
+*		3.float torque
+*		4.float scaleFactor
 *
 *     Function Description:
-*		Idle function for main module, checks that other functions are 
-*		being run, 
+*		Calculates the torque based of slipping factor and throttle position
+*		
 ***************************************************************************/
 	while(1) {
 
@@ -139,9 +163,7 @@ int mainModuleIdle() {
 				// apps - accelerator pedal position system
 				// bse - breake system encoder
 				apps_max = torque;
-				torque *= scaleFactor;
-				sendMotorMessage;
-				continue;
+				torque *= scaleFactor;	
 			}
 			else {
 				if (apps_max - torque > LC_THRESHOLD) {
@@ -149,23 +171,13 @@ int mainModuleIdle() {
 				}
 				else {
 					torque *= scaleFactor;
-					sendMotorMessage;
-					continue;
+					
 				}
 			}
 			
 		}
 
-		if (currentTime - MMPB_TIME > LC_THRESHOLD) {
-			torque = 0;
-			// low power state?
-			// error
-		}
-
-		if (currentTime - MMWM_TIME > LC_THRESHOLD) {
-			launchControl = 0;
-			//error
-		}
+		
 
 		sendMotorMessage();
 
@@ -173,12 +185,82 @@ int mainModuleIdle() {
 	}
 }
 
+int mainModuleTimeCheckIdle() {
+/***************************************************************************
+*
+*     Function Information
+*
+*     Name of Function: mainModuleTimeCheckIdle
+*
+*     Programmer's Name: Kai Strubel
+*
+*     Function Return Type: int
+*
+*     Parameters (list data type, name, and comment one per line):
+*       1.
+*       
+*      Global Dependents:
+*	    1.bool launchControl
+*		2.float MMPB_TIME time pedal box message handler function was last run 
+*		3.float MMWM_TIME time wheel module handler function was last run
+*		4.float torque
+*		5.float currentTime
+*
+*     Function Description:
+*		Checks if wheel module and pedal box are still communicating
+*		
+***************************************************************************/
+	while (1) {
+		if (currentTime - MMPB_TIME > LC_THRESHOLD) {
+			torque = 0;
+			// low power state?
+			// error
+		}
+		if (currentTime - MMWM_TIME > LC_THRESHOLD) {
+			launchControl = 0;
+			//error
+		}
+		vTaskDelay(500);
+	}
+}
+
+int heartbeatIdle() {
+/***************************************************************************
+*
+*     Function Information
+*
+*     Name of Function: heartbeatIdle
+*
+*     Programmer's Name: Kai Strubel
+*
+*     Function Return Type: int
+*
+*     Parameters (list data type, name, and comment one per line):
+*       1.
+*       
+*      Global Dependents:
+*
+*     Function Description:
+*		Heart beat to communicate that main module is alive
+*		
+***************************************************************************/
+	// write to GPIO
+	while (1) {
+		HAL_GPIO_WritePin(GPIOP, GPIO_PIN_E7, GPIO_PIN_SET);
+		vTaskDelay(500);
+	}
+	
+}
+
 int main(void) {
-	Gloabal_Queue_Handle = xQueueCreate(3, sizeof(Pedalbox_msg_t));
+
+	Global_Pedal_Box_Queue_Handle = xQueueCreate(3, sizeof(Pedalbox_msg_t));
 
 	/* Create Tasks */
 	xTaskCreate(pedalBoxMsgHandler, (signed char*) "pedalBoxMsgHandler", 1024, NULL, 1, NULL);
-	xTaskCreate(mainModuleIdle, (signed char*) "mainModuleIdle", 1024, NULL, 1, NULL);
+	xTaskCreate(mainModuleTimeCheckIdle, (signed char*) "mainModuleTimeCheckIdle", 1024, NULL, 1, NULL);
+	xTaskCreate(mainModuleTorque, (signed char*) "mainModuleTorque", 1024, NULL, 1, NULL);
+	xTaskCreate(heartbeatIdle, (signed char*) "heartbeatIdle", 1024, NULL, 1, NULL);
 
 	vTaskStartScheduler();
 
